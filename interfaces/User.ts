2905d1ce9285b3959d092412285fbec557190deb
TresorRw <alaintresorcyusa683@gmail.com>
2023-05-17 23:14:12 +0200

    Added required user interfaces

--- interfaces/User.ts
+++ interfaces/User.ts
@@ -3,3 +3,16 @@ export interface registerUserProps {
     display_name: string,
     password: string
 }
+
+export interface loginUserProps {
+    username: string,
+    password: string
+}
+
+export interface singleUserProps {
+    username: string
+}
+
+export interface userIdProps {
+    userId: number
+}
