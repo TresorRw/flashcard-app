b8cb3822edbb2a8e945a5e7b46a95da83c3fad3a
TresorRw <alaintresorcyusa683@gmail.com>
2023-05-19 10:11:15 +0200

    Removed plugins in Apollo-server

--- app.ts
+++ app.ts
@@ -1,18 +1,15 @@
 import express, { Application } from "express";
 import cors from "cors";
-import http from "http";
 import { config } from "dotenv";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { resolvers } from "./schemas/resolvers";
 import { typeDefs } from "./schemas/typeDefs";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import type { AppContext } from "./interfaces/AppContext";
 import { context } from "./utils/Context";
 config();
 
 const app: Application = express();
-const httpServer = http.createServer(app);
 const PORT: unknown = process.env.PORT;
 
 app.use(cors());
@@ -20,7 +17,6 @@ app.use(express.json());
 
 const server: ApolloServer = new ApolloServer<AppContext>({
     typeDefs, resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 
 server
